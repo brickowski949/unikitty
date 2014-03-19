7655b43b19ac776b02a0232f8465c64196e19306
brickowski949 <brickowski949@gmail.com>
2014-03-18 19:05:54 -0700

    Initial check in

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('unikittyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
